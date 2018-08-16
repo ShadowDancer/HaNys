6a6c99a0f5c4993656b0ebbda51ec842672a418a
Przemyslaw Onak <przemekonak@gmail.com>
2018-08-16 22:04:12 +0200

    Initial commit

---
+++ HaNys.Shared/Login/LoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HaNys.Shared
+{
+    public class LoginRequestDto
+    {
+        public string Username { get; }
+
+        public string Password { get; }
+    }
+}
